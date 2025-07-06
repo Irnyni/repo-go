3f617778780c2b44c61cbadfe73f7b4802f2e339
felipe malengo <you@example.com>
2025-07-06 00:38:24 -0300

    map go

---
+++ map.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func main() {
+	dados := map[string][]string{
+		"malengo": []string{
+			"surf", "bola,", "caminhada",
+		},
+		"joão": []string{
+			"nadar", "passear",
+		},
+		"carlos": []string{
+			"dormir", "comer",
+		},
+	}
+
+	for i, v := range dados {
+		fmt.Println("\n", i)
+		for _, item := range v {
+
+			println(item)
+
+		}
+
+	}
+
+	dados["debora"] = []string{
+
+		"passear",
+		"cozinhar",
+	}
+	for i, v := range dados {
+		fmt.Println("\n", i)
+		for _, item := range v {
+
+			println(item)
+
+		}
+
+	}
+
+	delete(dados, "debora")
+	for i, v := range dados {
+		fmt.Println("\n", i)
+		for _, item := range v {
+
+			println(item)
+
+		}
+
+	}
+}
