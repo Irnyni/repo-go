d0ef4000f19fe5a0b1c498c271be59adaac26974
Irnyni <felipemalengo1@hotmail.com>
2025-07-19 20:07:02 -0300

    Unmarshal json

---
+++ jason2.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+		"fmt"
+		"encoding/json"
+
+
+)
+
+
+
+type Carro struct {
+	Marca       string  `json:"Marca"`
+	Modelo      string  `json:"Modelo"`
+	Ano         string  `json:"Ano"`
+	Fipe        float64 `json:"Fipe"`
+	Combustivel string  `json:"Combustivel"`
+	Km          int     `json:"Km"`
+}
+
+
+
+func main(){
+sliceofbytes:=[]byte(`{"Marca":"fiat","Modelo":"argo","Ano":"2015","Fipe":45000.7,"Combustivel":"gasolina","Km":130000}`)
+var carros Carro
+err := json.Unmarshal(sliceofbytes,&carros)
+if err!= nil{
+		fmt.Println(err)
+}
+fmt.Println(carros.Modelo)
+
+}
