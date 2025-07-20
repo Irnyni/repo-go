40681cdff67192125b68e58707439bfcd2ddf56a
Irnyni <felipemalengo1@hotmail.com>
2025-07-20 12:58:10 -0300

    encoder 2

---
+++ encoder2.go
@@ -0,0 +1,27 @@
+package main
+
+import(
+		"fmt"
+		"encoding/json"
+		"os"
+
+)
+type Funcionario struct{
+
+		Nome string
+		Cargo string
+		Salario float64
+}
+
+func main(){
+
+	f1:= Funcionario{"carlo","torneiro",3500}
+	f2:= Funcionario{"flavio","ajustador",3000}
+	f3:= Funcionario{"frigo","encarregado",10000}
+
+funcionarios:= []Funcionario{f1,f2,f3}
+fmt.Println(funcionarios)
+encoder:=json.NewEncoder(os.Stdout)
+encoder.Encode (funcionarios)
+
+}
