81ac9145704de8d00c47bc7298a9160f91379082
Irnyni <felipemalengo1@hotmail.com>
2025-07-20 13:36:42 -0300

    go para json e vice versa

--- encoder2.go
+++ encoder2.go
@@ -20,10 +20,28 @@ func main(){
 	f3:= Funcionario{"frigo","encarregado",10000}
 
 funcionarios:= []Funcionario{f1,f2,f3}
-fmt.Println(funcionarios)
+fmt.Println("Slice normal:",funcionarios)
+fmt.Println("Saida os.Stdout: ")
 err:=json.NewEncoder(os.Stdout).Encode(funcionarios)
 if err != nil{
 	fmt.Println(err)
 }
+fmt.Println("Saída após encoder não salvando o dado:\n", funcionarios)
+a,err:= json.Marshal(funcionarios)
+if err != nil{
+	fmt.Println(err)
+}
+
+fmt.Println("Saida após marshal, criando a variavel a: \n",string(a))
+
+
+var f []Funcionario
+sliceb:= []byte(a)
+err1:= json.Unmarshal(sliceb,&f)
+if err != nil{fmt.Println(err1)}
+fmt.Println("Tranformação de json para go através do unmarshal:",f)
+
+
+
 
 }
