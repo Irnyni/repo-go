1ce52b532d453a85c201951fb875e8275f4d9393
Irnyni <felipemalengo1@hotmail.com>
2025-07-27 10:29:24 -0300

    sync

--- concorrencia.go
+++ concorrencia.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func main() {
-	segunda()
-	primeira()
+var wg sync.WaitGroup
 
+func main() {
+	wg.Add(2)
+	go segunda()
+	go primeira()
+	wg.Wait()
 }
 
 func primeira() {
 	for i := 0; i < 200; i++ {
 		fmt.Println(i)
 	}
-
+	wg.Done()
 }
 
 func segunda() {
@@ -22,5 +26,6 @@ func segunda() {
 		fmt.Println(a)
 
 	}
+	wg.Done()
 
 }
