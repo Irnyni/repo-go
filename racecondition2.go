55c8b739e3a331b28dab4dcb9df887a8849c2f7d
Irnyni <felipemalengo1@hotmail.com>
2025-07-27 14:04:21 -0300

    sem mutex

---
+++ racecondition2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var goroutine sync.WaitGroup
+var cont = 0
+
+func main() {
+	gerarGo(1000000)
+	fmt.Println("Total do contador:", cont)
+
+}
+
+func gerarGo(num int) {
+	goroutine.Add(num)
+	for i := 0; i < num; i++ {
+		go func() {
+			runtime.Gosched()
+
+			cont += 1
+			goroutine.Done()
+		}()
+
+	}
+	goroutine.Wait()
+}
