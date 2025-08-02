37ec0a7bd7d158324a4bad109d0adaf4e0decd6e
Irnyni <felipemalengo1@hotmail.com>
2025-08-02 15:21:01 -0300

    select3

---
+++ select3.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	par := make(chan int)
+	impar := make(chan int)
+	encerrar := make(chan string)
+	go enviar(par, impar, encerrar, 500)
+	receber(par, impar, encerrar)
+
+}
+func enviar(p, i chan int, e chan string, n int) {
+	for f := 0; f < n; f++ {
+		if f%2 == 0 {
+			p <- f
+
+		} else {
+			i <- f
+
+		}
+	}
+	e <- "encerrar"
+
+}
+
+func receber(p, i chan int, e chan string) {
+	for {
+		select {
+		case v := <-p:
+			fmt.Println("par:", v)
+		case v := <-i:
+			fmt.Println("impar:", v)
+		case <-e:
+			return
+		}
+	}
+
+}
