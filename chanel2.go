8a48c3079b0bbbc927a9a9d643309894c3fed13c
Irnyni <felipemalengo1@hotmail.com>
2025-08-10 16:49:07 -0300

    paralelismo

--- chanel2.go
+++ chanel2.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 )
 
 var wg sync.WaitGroup
 
 func main() {
-
+	fmt.Println("goroutines:", runtime.NumGoroutine())
 	canal := make(chan int)
 	canal2 := make(chan int)
 	wg.Add(2)
 	go colocar(canal)
+	fmt.Println("Goroutines :", runtime.NumGoroutine())
 	go retirar(canal, canal2)
+	fmt.Println("Goroutines :", runtime.NumGoroutine())
+
 	go func() {
 		wg.Wait()
 		close(canal2)
 	}()
 	for v := range canal2 {
-		fmt.Println(v)
+		fmt.Printf("%v ", v)
 	}
-
+	fmt.Println("Goroutines :", runtime.NumGoroutine())
 }
 
 func colocar(canal chan int) {
