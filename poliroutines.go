ff96a127089fc76b1aa4d12fd20bae7294032142
Irnyni <felipemalengo1@hotmail.com>
2025-08-10 17:54:15 -0300

    goroutines

---
+++ poliroutines.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+
+	routine := 10
+	c := make(chan int)
+	wg.Add(routine)
+	criago(routine, c)
+
+	go func() {
+		wg.Wait()
+		close(c)
+
+	}()
+	for v := range c {
+		fmt.Printf("\nNúmeros adicionados no canal:\t%v", v)
+	}
+}
+
+func criago(n int, c chan int) {
+
+	for i := 0; i < n; i++ {
+
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 10; i++ {
+				c <- i
+
+			}
+
+		}()
+	}
+}
