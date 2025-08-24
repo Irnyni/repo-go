0dcf4b50163cec8ffefe72eab7378cca52171e09
Irnyni <felipemalengo1@hotmail.com>
2025-08-24 14:54:14 -0300

    db

--- server/main.go
+++ server/main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
+var db *sql.DB
 
-	http.HandleFunc("/testando", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "ola sou do servidor")
-	})
+type Video struct {
+	Titulo    string `json:"titulo"`
+	Descricao string `json:"descricao"`
+	Imagem    string `json:"imagem"`
+}
+
+func main() {
+	var err error
+
+	db, err = sql.Open("sqlite3", "./videos.db")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS videos(
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			titulo TEXT,
+			descricao TEXT,
+			imagem TEXT
+	)`)
+	if err != nil {
+		fmt.Println(err)
+	}
 	http.HandleFunc("/pagina_inicial", paginaInicial)
 	http.HandleFunc("/videos", videos)
 	http.HandleFunc("/sinopse", sinopses)
-
 	fmt.Println("server iniciado")
-	err := http.ListenAndServe(":8085", nil)
+	err = http.ListenAndServe(":8085", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -26,7 +50,43 @@ func paginaInicial(w http.ResponseWriter, r *http.Request) {
 }
 
 func videos(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "PAGINA DE VIDEOS")
+	switch r.Method {
+	case http.MethodPost:
+		var v Video
+		err := json.NewDecoder(r.Body).Decode(&v)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		_, err = db.Exec(`INSERT INTO videos(Titulo,Descricao,Imagem) 
+		VALUES(?,?,?)`, v.Titulo, v.Descricao, v.Imagem)
+		if err != nil {
+			fmt.Println("Erro ao salvar no banco")
+		}
+	case http.MethodGet:
+		rows, err := db.Query(`SELECT titulo, descricao, imagem FROM videos`)
+		if err != nil {
+			fmt.Println(err)
+			defer rows.Close()
+
+		}
+		var videos []Video
+		for rows.Next() {
+			var v Video
+			err := rows.Scan(&v.Titulo, &v.Descricao, &v.Imagem)
+			if err != nil {
+				http.Error(w, "Erro ao ler dados do banco", http.StatusInternalServerError)
+				return
+			}
+			videos = append(videos, v)
+		}
+
+		// Retorna os dados como JSON
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(videos)
+
+	}
+
 }
 
 func sinopses(w http.ResponseWriter, r *http.Request) {
